081f026140f50c650ff69f446f304d3d01a4027d
Marcus Edel <marcus.edel@collabora.com>
2026-02-19 12:18:24 -0500

    Make sure we report the supported ops + datatypes.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- ggml/src/ggml-metal/ggml-metal-device.m
+++ ggml/src/ggml-metal/ggml-metal-device.m
@@ -1250,7 +1250,7 @@ bool ggml_metal_device_supports_op(ggml_metal_device_t dev, const struct ggml_te
                 };
             }
         case GGML_OP_GET_ROWS:
-            return op->src[0]->type != GGML_TYPE_NVFP4;
+            return op->src[0]->type != GGML_TYPE_NVFP4 && op->src[0]->type != GGML_TYPE_TQ1_0;
         case GGML_OP_SET_ROWS:
             {
                 if (op->src[0]->type != GGML_TYPE_F32) {
