34bb9874b6b6ee154341cfc9be7f39ede4c6bf43
Marcus Edel <marcus.edel@collabora.com>
2026-05-13 23:48:29 +0000

    Fix Metal soft_max_back/rms_norm_back precision by dropping the nth=std::min(nth, ne00[/4]) clamps.
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>
    (cherry picked from commit a26fa7117b211e070e0fa41e240c113f70a6ab60)

--- ggml/src/ggml-metal/ggml-metal-ops.cpp
+++ ggml/src/ggml-metal/ggml-metal-ops.cpp
@@ -5340,12 +5340,10 @@ int ggml_metal_op_soft_max_back(ggml_metal_op_t ctx, int idx) {
         while (nth < ne00_4 && nth < ggml_metal_pipeline_max_theads_per_threadgroup(pipeline)) {
             nth *= 2;
         }
-        nth = std::min(nth, ne00_4);
     } else {
         while (nth < ne00 && nth < ggml_metal_pipeline_max_theads_per_threadgroup(pipeline)) {
             nth *= 2;
         }
-        nth = std::min(nth, ne00);
     }
 
     nth = std::max(1, nth);
@@ -5408,7 +5406,6 @@ int ggml_metal_op_rms_norm_back(ggml_metal_op_t ctx, int idx) {
     }
 
     nth = std::min(nth, ggml_metal_pipeline_max_theads_per_threadgroup(pipeline));
-    nth = std::min(nth, ne00/4);
 
     const size_t smem = pipeline.smem;
 
