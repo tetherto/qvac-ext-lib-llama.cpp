28e264ef815e645c00beaf3537ab64acbe33f482
Marcus Edel <marcus.edel@collabora.com>
2026-06-22 13:24:16 +0000

    ggml-rpc: account for new backward ops (GGML_OP_COUNT 100->105), bump proto patch version
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- ggml/include/ggml-rpc.h
+++ ggml/include/ggml-rpc.h
@@ -11,7 +11,7 @@ extern "C" {
 #define RPC_PROTO_PATCH_VERSION    2
 
 #ifdef  __cplusplus
-static_assert(GGML_OP_COUNT == 101, "GGML_OP_COUNT has changed - update RPC_PROTO_PATCH_VERSION");
+static_assert(GGML_OP_COUNT == 106, "GGML_OP_COUNT has changed - update RPC_PROTO_PATCH_VERSION");
 #endif
 
 #define GGML_RPC_MAX_SERVERS       16
