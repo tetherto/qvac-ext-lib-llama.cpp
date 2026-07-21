433035324b06de7c30749fb75689022702af532a
Marcus Edel <marcus.edel@collabora.com>
2026-07-21 15:54:53 -0400

    common: disable weight repacking for training loads, backward ops can't read repacked layouts
    Signed-off-by: Marcus Edel <marcus.edel@collabora.com>

--- common/common.cpp
+++ common/common.cpp
@@ -1654,7 +1654,10 @@ struct llama_model_params common_model_params_to_llama(common_params & params) {
     mparams.use_direct_io   = params.use_direct_io;
     mparams.use_mlock       = params.use_mlock;
     mparams.check_tensors   = params.check_tensors;
-    mparams.use_extra_bufts = !params.no_extra_bufts;
+
+    // Disable weight repacking for training loads, backward ops can't read
+    // repacked layouts.
+    mparams.use_extra_bufts = !params.no_extra_bufts && !params.training;
     mparams.no_host         = params.no_host;
 
     if (params.kv_overrides.empty()) {
