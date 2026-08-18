102bca86260e1e3737269e7fc57b8a37d24a5cff
Korcan Hussein <korcan.hussein@collabora.com>
2026-08-18 11:07:11 +0100

    ggml-cuda: implement ssm-conv-back-c
    (cherry picked from commit 675011d4242461c6a437910194f610751a7b7f1a)

--- ggml/src/ggml-cuda/ssm-conv-back.cuh
+++ ggml/src/ggml-cuda/ssm-conv-back.cuh
@@ -3,3 +3,6 @@
 
 __host__ void
 ggml_cuda_op_ssm_conv_back_sx(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
+
+__host__ void
+ggml_cuda_op_ssm_conv_back_c(ggml_backend_cuda_context & ctx, ggml_tensor * dst);
