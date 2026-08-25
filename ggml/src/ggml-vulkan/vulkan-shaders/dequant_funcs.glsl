dev-nid/qvac-fabric-llm.cpp

ggml-vulkan: TurboQuant vulkan backend and shader integration
Vulkan backend for the TurboQuant (TBQ3_0/TBQ4_0) and PolarQuant
(PQ3_0/PQ4_0) KV-cache quant formats (block=128 and block=64), ported
onto the b9518 flash-attention architecture.

b9518 already carries a per-K/V-type quantized-KV flash-attention design
(the FaTypeK/FaTypeV runtime spec-constant model), which differs from the
b9341-era TurboQuant import's compile-time per-type model. Rather than
revert b9518's FA, this integrates both:

- Generic FA (F16/BF16/Q4_0/Q4_1/Q5_0/Q5_1/Q8_0/Q1_0) keeps b9518's
  runtime FaTypeK/V decode unchanged. The shared FA shaders gate it behind
  LLAMA_UPSTREAM_FA_MIXED_TYPES, which vulkan-shaders-gen.cpp defines for
  the generic variants.
- TBQ/PQ ride on dedicated per-type compile-time SPIR-V modules
  (flash_attn_f32_f16_<k>[_<v>][_cm1|_cm2]) that include
  turbo-quant/flash_attn_base.glsl for the bit-packed codebook decode and
  the QJL (Johnson-Lindenstrauss) Stage-2 correction. ggml-vulkan's three
  FA pipeline loops select these per (k_type, v_type) via CREATE_FA_TBQ.

The QJL correction is applied to the Q*K^T score for TurboQuant K only
(matching the CPU vec_dot reference); PolarQuant and all V decode are
Stage-1 (codebook x scale). Same-type and mixed K/V (TBQ/PQ on K; PQ,
Q4_0, Q8_0 or F16 on V) are supported on scalar, coopmat1 and coopmat2.

Also includes: TBQ/PQ mul_mat (mul_mat_vec FHT/QJL epilogue + standalone
mul_mm QJL correction pass), dequant, copy_to/from_quant + set_rows
(incl. cooperative TBQ_COOP and norm-correction variants), the Metal
MUL_MAT/MUL_MAT_ID reject for TBQ/PQ, llama-bench cache-type parsing, the
test-backend-ops TBQ/PQ MUL_MAT + FLASH_ATTN_EXT sweeps, and the
test-turboquant.sh / test-copy-tbq-subgroups / kv-cache perf+perp harness.

The scalar FlashAttn MMQ (_int8) path is disabled fork-wide: the import
reworked the scalar shader's MMQ path into the compile-time turbo-quant
decode, so the generic _int8 module no longer compiles; the 

ggml/src/ggml-vulkan/vulkan-shaders/dequant_funcs.glsl
@@ -572,6 +572,8 @@ vec4 dequantize4(uint ib, uint iqs, uint a_offset) {
 }
 #endif
 
+#include "turbo-quant/dequant_funcs.glsl"
+
 #if defined(DATA_A_F32) || defined(DATA_A_F16) || defined(DATA_A_BF16)
 vec2 get_dm(uint ib, uint a_offset) {
     return vec2(0, 0);
@@ -587,7 +589,8 @@ vec2 get_dm(uint ib, uint a_offset) {
 }
 #endif
 
-#if defined(DATA_A_Q4_0) || defined(DATA_A_Q5_0) || defined(DATA_A_Q8_0) || defined(DATA_A_TQ2_0) || defined(DATA_A_TQ1_0) || defined(DATA_A_IQ1_S) || defined(DATA_A_IQ2_XXS) || defined(DATA_A_IQ2_XS) || defined(DATA_A_IQ2_S) || defined(DATA_A_IQ3_XXS) || defined(DATA_A_IQ3_S) || defined(DATA_A_IQ4_XS) || defined(DATA_A_IQ4_NL)
+#if defined(DATA_A_Q4_0) || defined(DATA_A_Q5_0) || defined(DATA_A_Q8_0) || defined(DATA_A_TQ2_0) || defined(DATA_A_TQ1_0) || defined(DATA_A_IQ1_S) || defined(DATA_A_IQ2_XXS) || defined(DATA_A_IQ2_XS) || defined(DATA_A_IQ2_S) || defined(DATA_A_IQ3_XXS) || defined(DATA_A_IQ3_S) || defined(DATA_A_IQ4_XS) || defined(DATA_A_IQ4_NL) || \
+    defined(DATA_A_ANY_PQ3_OR_4_0) || defined(DATA_A_ANY_TBQ3_OR_4_0)
 vec2 get_dm(uint ib, uint a_offset) {
     return vec2(float(data_a[a_offset + ib].d), 0);
 }

tetherto/qvac-ext-lib-llama.cpp

Co-authored-by: Guilherme Gallo <guilherme.gallo@collabora.com>
(cherry picked from commit beddffc2f7bee74721de2003435d031a7a7bb116)
---
b9692 rebase:
- Gate the TBQ/PQ _64 shader emission on !dot2: on the dot2 passes this
  block re-emits the non-dot2 names and double-defines every _64 shader.
b9840 rebase:
- upstream 0eb874d374 ("vulkan: make mul_mm ALIGNED a spec constant",
  #24689, lands at b9771) collapsed those into a single shader per type
  -- alignment is now a runtime ALIGNED spec constant, and mul_mm.comp
  gained a B_TYPE_SCALAR / B_SCALAR binding so the one shader can do
  both the vectorized (aligned) and scalar (unaligned) B loads. Rebased
  onto b9840, the TurboQuant block still emits the old
  _f32/_f32_aligned/_f16/_f16_aligned variants with
  load_vec_a_unaligned, which no longer matches the shared mul_mm.comp.
   - Fold this in so each TQ/PQ type emits one shader carrying
     B_TYPE_SCALAR and drops the _aligned variant, exactly like
     upstream's _128 block.
b9899 rebase:
- upstream wrapped the SET_ROWS accepted-type switch in
  ggml_backend_vk_device_supports_op in an outer
  `if (op->src[0]->type == GGML_TYPE_F32)` guard and keeps a direct
  `return true;` for the standard quant types. Re-slot the TBQ/PQ
  pipeline-null gating cases into the new switch and drop the old
  `break;` + trailing-`return true;` fallthrough, which no longer fits.
b9952 rebase:
- Collapse the four `SET_ROWS_TQ*` pipeline macros into one
  parameterized macro and replace the 4-way TBQ/PQ-64 type test with
  `ggml_is_tbq_or_pq_64()`; skip emitting `fa_mixed` shaders where K/V
  share a type (already covered by the single-type pipelines)
- dedup the kv-quant perf/perp test scripts into the shared
  `kv_quant_common.sh` / `kv_quant_agg_common.py`.
- Compute the TBQ QJL correction-pass gate and its B-source choice once
  (`tbq_qjl_pass` / `qjl_use_f32_b_from_src1` / `qjl_b_is_f16`) and
  share them between the descriptor pre-request and the dispatch,
  including the ne

ggml/src/ggml-vulkan/vulkan-shaders/dequant_funcs.glsl
@@ -591,6 +591,8 @@ vec4 dequantize4(uint ib, uint iqs, uint a_offset) {
 }
 #endif
 
+#include "turbo-quant/dequant_funcs.glsl"
+
 #if defined(DATA_A_F32) || defined(DATA_A_F16) || defined(DATA_A_BF16)
 vec2 get_dm(uint ib, uint a_offset) {
     return vec2(0, 0);
@@ -606,7 +608,8 @@ vec2 get_dm(uint ib, uint a_offset) {
 }
 #endif
 
-#if defined(DATA_A_Q2_0) || defined(DATA_A_Q4_0) || defined(DATA_A_Q5_0) || defined(DATA_A_Q8_0) || defined(DATA_A_TQ1_0) || defined(DATA_A_IQ1_S) || defined(DATA_A_IQ2_XXS) || defined(DATA_A_IQ2_XS) || defined(DATA_A_IQ2_S) || defined(DATA_A_IQ3_XXS) || defined(DATA_A_IQ3_S) || defined(DATA_A_IQ4_XS) || defined(DATA_A_IQ4_NL)
+#if defined(DATA_A_Q2_0) || defined(DATA_A_Q4_0) || defined(DATA_A_Q5_0) || defined(DATA_A_Q8_0) || defined(DATA_A_TQ1_0) || defined(DATA_A_IQ1_S) || defined(DATA_A_IQ2_XXS) || defined(DATA_A_IQ2_XS) || defined(DATA_A_IQ2_S) || defined(DATA_A_IQ3_XXS) || defined(DATA_A_IQ3_S) || defined(DATA_A_IQ4_XS) || defined(DATA_A_IQ4_NL) || \
+    defined(DATA_A_ANY_PQ3_OR_4_0) || defined(DATA_A_ANY_TBQ3_OR_4_0)
 vec2 get_dm(uint ib, uint a_offset) {
     return vec2(float(data_a[a_offset + ib].d), 0);
 }